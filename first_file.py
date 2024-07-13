59713ff1acdbc10a86348bc9397bae5d543933b7
nglis1 <113645856+nglis1@users.noreply.github.com>
2024-07-13 15:10:05 -0500

    Update first_file.py

--- first_file.py
+++ first_file.py
@@ -1,2 +1,7 @@
-print('hello')
-print('holass')
+first = input('Enter your First Name:')
+last = input('Enter your Last Name:')
+age = input('Enter your age:')
+age = int(age)
+
+newAge = (100 - age) + 2024
+print(first + ' ' + last + ' ' + 'will turn 100 years old in ' + str(newAge))
