c7cd476d4d46ab052b0d21d675e4a3b72962068b
sk1nnyking06 <laidaniel06@gmail.com>
2024-07-13 15:43:18 -0500

    Update sk1nnyevenodd.py

--- sk1nnyevenodd.py
+++ sk1nnyevenodd.py
@@ -1,5 +1,14 @@
 num = int(input("Enter a number: "))
+divisor = int(input("Enter a divisor: "))
 if num % 2 == 0:
-    print("The number is even.")
+    if num % 4 == 0:
+        print("The number is even and a multiple of 4.")
+    else:
+        print("The number is even.")
 else:
     print("The number is odd.")
+
+if num % divisor == 0:
+    print(f"{num} is divisible by {divisor}.")
+else:
+    print(f"{num} is not divisible by {divisor}.")
