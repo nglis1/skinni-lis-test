86b02e4cbcd424a74d10b5cba613c9f9dd67da88
nglis1 <113645856+nglis1@users.noreply.github.com>
2024-07-13 15:50:46 -0500

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,6 +1,14 @@
-num = input('Enter a number : ')
+num = input('Enter a number: ')
+check = input('Enter a divisor: ')
 
-if int(num)%2 == 0:
+if int(num)%4 == 0 and int(num) > 4:
+    print(f'{num} is a multiple of 4.\n')
+elif int(num)%2 == 0:
     print(f'{num} is an even number.\n')
 else:
     print(f'{num} is an odd number. \n')
+
+if int(num)%int(check) == 0:
+    print(f'{check} divides evenly into {num}.\n')
+else:
+    print(f'{check} does not evenly divide into {num}.\n')
