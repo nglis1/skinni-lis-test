38bc73b5f7edd70399d718f8c3be15e5e1ee408c
sk1nnyking06 <laidaniel06@gmail.com>
2024-07-13 15:11:28 -0500

    Update sk1nny.py

--- sk1nny.py
+++ sk1nny.py
@@ -1,1 +1,10 @@
 print("sk1nny was here")
+from datetime import datetime
+current_year = datetime.now().year
+
+name = input("What is your name? ")
+age = input("What is your age? ")
+
+print(f"Hello {name}! You are {age} years old.")
+year100 = current_year + (100 - int(age))
+print(f"You will turn 100 years old in the year {year100}.")
